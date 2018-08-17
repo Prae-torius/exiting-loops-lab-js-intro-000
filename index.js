17b97a418ff9648bb95854395ca562948f5ad883
Preston A Arnette <arnettepreston@gmail.com>
2018-08-17 01:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === findFn) {
-      return array[i]
+      return array[i];
     } else {
-      return null
+      return null;
     }
   }
 }
