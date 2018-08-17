52be81d7a0682cfda642ab1fb65465848be77e20
Preston A Arnette <arnettepreston@gmail.com>
2018-08-17 01:16:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i <= array.length; ++i) {
+  for (let i = 0; i < array.length; ++i) {
     if (array[i] === stopValue) {
       break;
     } else {
-      array[i] = changeValue
+      array[i] = changeValue;
     }
   }
 }
